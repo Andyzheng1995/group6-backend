4cf6abaef8eb7f5cb12fbe21ab2947fc4d11f8ff
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-16 18:37:17 -0500

    hello

--- src/main/java/mcgill/ca/ecse321/group6backend/Group6BackendApplication.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/Group6BackendApplication.java
@@ -1,12 +1,20 @@
 package mcgill.ca.ecse321.group6backend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class Group6BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group6BackendApplication.class, args);
 	}
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 }
