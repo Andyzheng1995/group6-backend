566dae9ca99459add92238ab5a4220dd5485c16d
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-17 18:05:13 -0500

    added controller

--- src/main/java/mcgill/ca/ecse321/group6backend/Group6BackendApplication.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/Group6BackendApplication.java
@@ -2,19 +2,11 @@ package mcgill.ca.ecse321.group6backend;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
 @SpringBootApplication
 public class Group6BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group6BackendApplication.class, args);
 	}
-
-  @RequestMapping("/")
-  public String greeting(){
-    return "Hello world!";
-  }
 }
--- src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
@@ -1,6 +1,34 @@
 package mcgill.ca.ecse321.group6backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import mcgill.ca.ecse321.group6backend.model.User;
+import mcgill.ca.ecse321.group6backend.repository.UserRepository;
+
+@RestController
 public class UserController {
-
+	
+	@Autowired
+	UserRepository uRepository;
+	
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello world!";
+	}
+	
+	@PostMapping("/user/{name}")
+	public String createUser(@PathVariable("name") String name) {
+		User user = uRepository.createUser(name);
+		return user.getName();
+	}
+	
+	@RequestMapping("/user/{name}")
+	public User getUser(@PathVariable("name") String name) {
+		User user = uRepository.getUser(name);
+		return user;
+	}
 }
