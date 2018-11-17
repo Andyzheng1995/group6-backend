f00f1b975ee469f58afae91d995e0ec1c7fdea7a
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-17 18:32:32 -0500

    add more annotations on model

--- src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package mcgill.ca.ecse321.group6backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import mcgill.ca.ecse321.group6backend.model.User;
 import mcgill.ca.ecse321.group6backend.repository.UserRepository;
 
+@CrossOrigin
 @RestController
+@RequestMapping("api/user")
 public class UserController {
 	
 	@Autowired
@@ -20,13 +23,13 @@ public class UserController {
 		return "Hello world!";
 	}
 	
-	@PostMapping("/user/{name}")
+	@PostMapping("/register/{name}")
 	public String createUser(@PathVariable("name") String name) {
 		User user = uRepository.createUser(name);
 		return user.getName();
 	}
 	
-	@RequestMapping("/user/{name}")
+	@RequestMapping("/check/{name}")
 	public User getUser(@PathVariable("name") String name) {
 		User user = uRepository.getUser(name);
 		return user;
