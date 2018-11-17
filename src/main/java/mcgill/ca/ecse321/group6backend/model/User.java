1292ffd47769899f85c5fe2db918f31c941cf6cc
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-17 17:22:23 -0500

    add first model

---
+++ src/main/java/mcgill/ca/ecse321/group6backend/model/User.java
@@ -0,0 +1,32 @@
+package mcgill.ca.ecse321.group6backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	private long id;
+	private String name;
+	
+	@Id
+	@Column(name="Id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
