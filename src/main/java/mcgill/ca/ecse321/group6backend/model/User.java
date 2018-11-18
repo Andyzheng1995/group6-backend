f387928646a6b38cd4ec919dfefc8f6680612244
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-17 19:56:28 -0500

    update

--- src/main/java/mcgill/ca/ecse321/group6backend/model/User.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/model/User.java
@@ -5,15 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
-@NamedQueries({
-    @NamedQuery(name = "user.findAll", query = "SELECT e FROM user e")
-})
 public class User {
 	
 	private int id;
