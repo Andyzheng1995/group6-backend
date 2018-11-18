4f0e95056a65fe5731a784e6606e84ed2ee7693a
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-17 19:14:16 -0500

    change createUser

--- src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/controller/UserController.java
@@ -1,10 +1,16 @@
 package mcgill.ca.ecse321.group6backend.controller;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import mcgill.ca.ecse321.group6backend.model.User;
@@ -22,10 +28,21 @@ public class UserController {
 		return "Hello world!";
 	}
 	
-	@RequestMapping("/register/{name}")
-	public String createUser(@PathVariable("name") String name) {
-		User user = uRepository.createUser(name);
-		return user.getName();
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> createUser(
+            @RequestParam("username") String userName
+    ) throws JSONException {
+        User user = uRepository.createUser(userName);
+        if (user != null) {
+            JSONObject json = new JSONObject();
+            json.put("msg","data"+" " + userName + " created!");
+            return new ResponseEntity<>(json.toString(), HttpStatus.OK);
+        } else {
+            JSONObject json = new JSONObject();
+            json.put("msg","data" + " " + userName + " could not be created.");
+            return new ResponseEntity<>(json.toString(), HttpStatus.CONFLICT);
+        }
 	}
 	
 	@RequestMapping(value = "/check/{name}", method = RequestMethod.GET)
