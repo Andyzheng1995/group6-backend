663756e7abb36a0fc5e8fbc6aab5936dc1af29a2
Andy Zheng <zhaoyi.zheng@mail.mcgill.ca>
2018-11-19 13:53:47 -0500

    real user model

--- src/main/java/mcgill/ca/ecse321/group6backend/model/User.java
+++ src/main/java/mcgill/ca/ecse321/group6backend/model/User.java
@@ -13,6 +13,13 @@ public class User {
 	
 	private int id;
 	private String name;
+	private String phone;
+	private String pwd;
+	private int role;
+	private boolean currentTrip = false;
+	private String currentTripId = "";
+	private double score = 4;
+	private int totalNo = 0;
 	
 	@Id
 	@Column(name="id", updatable = false, nullable = false)
@@ -30,6 +37,55 @@ public class User {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	@Column(name="phone")
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Column(name="pwd")
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	@Column(name="role")
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	@Column(name="current_at_trip")
+	public boolean currentAtTrip() {
+		return currentTrip;
+	}
+	public void setCurrentAtTrip(boolean currentTrip) {
+		this.currentTrip = currentTrip;
+	}
+	@Column(name="current_trip_id")
+	public String getCurrentTripId() {
+		return currentTripId;
+	}
+	public void setCurrentTripId(String currentTripId) {
+		this.currentTripId = currentTripId;
+	}
+	@Column(name="score")
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	@Column(name="total_no")
+	public int getTotalNo() {
+		return totalNo;
+	}
+	public void setTotal_no(int totalNo) {
+		this.totalNo = totalNo;
 	}
 	
 	
